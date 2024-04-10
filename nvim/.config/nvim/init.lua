f218ebc68d60a445c79a4c917875f048111bca10
Josh Allan <joshua.allan92@gmail.com>
2024-04-11 07:50:20 +1000

    fix polling interval for gitsigns

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -282,12 +282,11 @@ require("lazy").setup({
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-					delay = 1000,
-					ignore_whitespace = false,
+					delay = 10,
+					ignore_whitespace = true,
 					virt_text_priority = 100,
 				},
 				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-
 			})
 		end,
 
