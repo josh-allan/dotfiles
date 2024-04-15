6a1bc09032c15f3d04dbcd75f073480486771a73
Josh Allan <joshua.allan92@gmail.com>
2024-04-15 17:44:08 +1000

    nvim tree keymappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,8 @@ if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+-- Source plugin configs
 require("lazy").setup("plugins")
 
 -- Source keymaps and options
