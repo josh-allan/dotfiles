e3208540a92f62a26b9da3bc8c6d9fa129971b6b
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-04-15 16:39:23 +1000

    Refactor neovim to a modular system

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,491 @@
+return {
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = true },
+			{ "williamboman/mason-lspconfig.nvim", opts = true },
+		},
+		opts = {
+			ensure_installed = {
+				"pyright", -- LSP for python
+				"ruff-lsp", -- linter for python (includes flake8, pep8, etc.)
+				"debugpy", -- debugger
+				"black", -- formatter
+				"isort", -- organize imports
+				"taplo", -- LSP for toml (for pyproject.toml files)
+			},
+		},
+	},
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+	-- autopairs config
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
+
+	-- Nvterm installation
+	{
+		"NvChad/nvterm",
+		config = function()
+			require("configs.nvterm")
+		end,
+	},
+
+	-- Gitsigns
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("configs.gitsigns")
+		end,
+
+		event = { "BufReadPre", "BufNewFile" },
+	},
+	--
+	--
+	-- Then, because we use the `config` key, the configuration only runs
+	-- after the plugin has been loaded:
+	--  config = function() ... end
+
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		config = function() -- This is the function that runs, AFTER loading
+			require("configs.which-key")
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+
+	-- Tab line
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{ "<C-,>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<C-.>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		},
+		opts = {
+			options = {
+      -- stylua: ignore
+              close_command = function(n)
+                    require("mini.bufremove").delete(n, false)
+            end,
+      -- stylua: ignore
+      right_mouse_command = function(n)
+                    require("mini.bufremove").delete(n, false)
+            end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = true,
+				-- diagnostics_indicator = function(_, _, diag)
+				-- 	local icons = require("lazyvim.config").icons.diagnostics
+				-- 	local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+				-- 		.. (diag.warning and icons.Warn .. diag.warning or "")
+				-- 	return vim.trim(ret)
+				-- end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("bufferline").setup(opts)
+			-- Fix bufferline when restoring a session
+			vim.api.nvim_create_autocmd("BufAdd", {
+				callback = function()
+					vim.schedule(function()
+						pcall(nvim_bufferline)
+					end)
+				end,
+			})
+		end,
+	},
+
+	-- Git setup
+	{ "tpope/vim-fugitive" },
+	{ -- Fuzzy Finder (files, lsp, etc)
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+				"nvim-telescope/telescope-fzf-native.nvim",
+
+				-- `build` is used to run some command when the plugin is installed/updated.
+				-- This is only run then, not every time Neovim starts up.
+				build = "make",
+
+				-- `cond` is a condition used to determine whether this plugin should be
+				-- installed and loaded.
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+
+			-- Useful for getting pretty icons, but requires a Nerd Font.
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		},
+		config = function()
+			require("configs.telescope")
+		end,
+	},
+
+	{ -- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs and related tools to stdpath for Neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+			-- Useful status updates for LSP.
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", opts = {} },
+
+			-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+			-- used for completion, annotations and signatures of Neovim apis
+			{ "folke/neodev.nvim", opts = {} },
+		},
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+
+	{ -- Autoformat
+		"stevearc/conform.nvim",
+		lazy = false,
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				-- Disable "format_on_save lsp_fallback" for languages that don't
+				-- have a well standardized coding style. You can add additional
+				-- languages here or re-enable it for the disabled ones.
+				local disable_filetypes = { c = true, cpp = true }
+				return {
+					timeout_ms = 500,
+					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				}
+			end,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform can also run multiple formatters sequentially
+				python = { "ruff" },
+				terraform = { "terraform fmt" },
+				shell = { "beautysh" },
+				--
+				-- You can use a sub-list to tell conform to run *until* a formatter
+				-- is found.
+				-- javascript = { { "prettierd", "prettier" } },
+			},
+		},
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("configs.noice")
+		end,
+	},
+
+	{ -- Autocompletion
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			{
+				"L3MON4D3/LuaSnip",
+				build = (function()
+					-- Build Step is needed for regex support in snippets.
+					-- This step is not supported in many windows environments.
+					-- Remove the below condition to re-enable on windows.
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+				dependencies = {
+					-- `friendly-snippets` contains a variety of premade snippets.
+					--    See the README about individual language/framework/plugin snippets:
+					--    https://github.com/rafamadriz/friendly-snippets
+					-- {
+					--   'rafamadriz/friendly-snippets',
+					--   config = function()
+					--     require('luasnip.loaders.from_vscode').lazy_load()
+					--   end,
+					-- },
+				},
+			},
+			"saadparwaiz1/cmp_luasnip",
+
+			-- Adds other completion capabilities.
+			--  nvim-cmp does not ship with all sources by default. They are split
+			--  into multiple repos for maintenance purposes.
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			require("configs.cmp")
+		end,
+	},
+
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start configs.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"nvim-telescope/telescope.nvim",
+			"mfussenegger/nvim-dap-python",
+		},
+		opts = {
+			dap_enabled = true, -- makes the debugger work with venv
+		},
+	},
+	-- Docstring creation
+	-- - quickly create docstrings via `<leader>a`
+	{
+		"danymat/neogen",
+		opts = true,
+		keys = {
+			{
+				"<leader>a",
+				function()
+					require("neogen").generate()
+				end,
+				desc = "Add Docstring",
+			},
+		},
+	},
+
+	-- better indentation behavior
+	-- by default, vim has some weird indentation behavior in some edge cases,
+	-- which this plugin fixes
+	{ "Vimjas/vim-python-pep8-indent" },
+	-- DEBUGGING
+
+	-- DAP Client for nvim
+	-- - start the debugger with `<leader>dc`
+	-- - add breakpoints with `<leader>db`
+	-- - terminate the debugger `<leader>dt`
+	{
+		"mfussenegger/nvim-dap",
+		keys = {
+			{
+				"<leader>dc",
+				function()
+					require("dap").continue()
+				end,
+				desc = "Start/Continue Debugger",
+			},
+			{
+				"<leader>db",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "Add Breakpoint",
+			},
+			{
+				"<leader>dt",
+				function()
+					require("dap").terminate()
+				end,
+				desc = "Terminate Debugger",
+			},
+		},
+	},
+
+	-- UI for the debugger
+	-- - the debugger UI is also automatically opened when starting/stopping the debugger
+	-- - toggle debugger UI manually with `<leader>du`
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = "mfussenegger/nvim-dap",
+		keys = {
+			{
+				"<leader>du",
+				function()
+					require("dapui").toggle()
+				end,
+				desc = "Toggle Debugger UI",
+			},
+		},
+		-- automatically open/close the DAP UI when starting/stopping the debugger
+		config = function()
+			local listener = require("dap").listeners
+			listener.after.event_initialized["dapui_config"] = function()
+				require("dapui").open()
+			end
+			listener.before.event_terminated["dapui_config"] = function()
+				require("dapui").close()
+			end
+			listener.before.event_exited["dapui_config"] = function()
+				require("dapui").close()
+			end
+		end,
+	},
+
+	-- Configuration for the python debugger
+	-- - configures debugpy for us
+	-- - uses the debugpy installation from mason
+	{
+		"mfussenegger/nvim-dap-python",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function()
+			-- uses the debugypy installation by mason
+			local debugpyPythonPath = require("mason-registry").get_package("debugpy"):get_install_path()
+				.. "/venv/bin/python3"
+			require("dap-python").setup(debugpyPythonPath, {})
+		end,
+	},
+	{ -- Collection of various small independent configs.modules
+		"echasnovski/mini.nvim",
+		config = function()
+			require("configs.mini")
+		end,
+	},
+	{ -- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"hcl",
+				"lua",
+				"luadoc",
+				"markdown",
+				"python",
+				"terraform",
+				"vim",
+				"vimdoc",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+				--  If you are experiencing weird indenting issues, add the language to
+				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+				additional_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
+		},
+		config = function(_, opts)
+			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup(opts)
+
+			-- There are additional nvim-treesitter modules that you can use to interact
+			-- with nvim-treesitter. You should go explore a few and see what interests you:
+			--
+			--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+			--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+		end,
+	},
+	require("configs.debug"),
+	require("configs.indent_line"),
+	require("configs.lint"),
+}, {
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+}
