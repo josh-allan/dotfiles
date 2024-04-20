f678977e3173ca0421430d26b779fd748a3823e8
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-04-20 12:10:39 +1000

    plugins cleanup and obsidian config

--- nvim/.config/nvim/lua/base/keymaps.lua
+++ nvim/.config/nvim/lua/base/keymaps.lua
@@ -23,10 +23,6 @@ keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnosti
 keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
 keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Disable arrow keys in normal mode
@@ -36,9 +32,7 @@ keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
--- Use CTRL+<hjkl> to switch between windows
 --
---  See `:help wincmd` for avlist of all window commands
 keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
