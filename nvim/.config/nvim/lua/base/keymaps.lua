34d73a5f759a0904e6a5549f8fe929d237634f57
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-05-01 07:39:07 +1000

    crufty macos path hack

--- nvim/.config/nvim/lua/base/keymaps.lua
+++ nvim/.config/nvim/lua/base/keymaps.lua
@@ -33,11 +33,11 @@ keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --
-keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
+-- keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+--
 keymap.set("n", "<C-,>", "<cmd>BufferPrevious<CR>")
 
 -- Move to previous/next
