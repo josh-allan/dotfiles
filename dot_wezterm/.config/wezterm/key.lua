466fb7a5b4753530cbb3500fa766df31b02d2102
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-05-01 16:27:43 +1000

    wezterm config

---
+++ dot_wezterm/.config/wezterm/key.lua
@@ -0,0 +1,132 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local function is_vim(pane)
+	local process_info = pane:get_foreground_process_info()
+	local process_name = process_info and process_info.name
+
+	return process_name == "nvim" or process_name == "vim"
+end
+
+local function find_vim_pane(tab)
+	for _, pane in ipairs(tab:panes()) do
+		if is_vim(pane) then
+			return pane
+		end
+	end
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+return {
+
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+
+	-- split panes
+	{
+		key = "-",
+		mods = "META",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "META",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+
+	-- Rename tab
+	{
+		key = "r",
+		mods = "CMD",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+
+	-- Toggle zoom for neovim
+	{
+		key = ";",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			local tab = window:active_tab()
+
+			-- Open pane below if current pane is vim
+			if is_vim(pane) then
+				if (#tab:panes()) == 1 then
+					-- Open pane below if when there is only one pane and it is vim
+					pane:split({ direction = "Bottom" })
+				else
+					-- Send `CTRL-; to vim`, navigate to bottom pane from vim
+					window:perform_action({
+						SendKey = { key = ";", mods = "CTRL" },
+					}, pane)
+				end
+				return
+			end
+
+			-- Zoom to vim pane if it exists
+			local vim_pane = find_vim_pane(tab)
+			if vim_pane then
+				vim_pane:activate()
+				tab:set_zoomed(true)
+			end
+		end),
+	},
+
+	-- Workspaces manager
+	{
+		key = "W",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+	},
+	-- Cmdline movement options
+	-- Move indivdual words with alt/opt
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
---
+++ dot_wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,76 @@
+local wezterm = require("wezterm")
+local keys = require("key")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font_size = 14.0
+config.line_height = 1.2
+
+config.window_decorations = "RESIZE"
+config.initial_rows = 45
+config.initial_cols = 200
+
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 22
+config.tab_bar_at_bottom = true
+
+config.keys = keys
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.font = wezterm.font("Fira Code")
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+
+config.color_scheme = "Tokyo Night"
+config.colors = require("color")
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 75
+
+local function tab_title(tab_info)
+	local title = tab_info.tab_title
+
+	if title and #title > 0 then
+		return title
+	end
+
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, cf, hover, max_width)
+	local title = tab_title(tab)
+
+	title = wezterm.truncate_left(title, max_width)
+	local i = tab.tab_index + 1
+
+	title = string.format(" %d %s ", i, title)
+
+	local background = "rgb(22, 24, 26 / 20%)"
+	local foreground = "white"
+
+	if tab.is_active then
+		background = "rgb(22, 24, 26 / 90%)"
+		foreground = "white"
+	end
+
+	return {
+		{ Background = { Color = "black" } },
+		{ Foreground = { Color = "black" } },
+		{ Text = "" },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = "black" } },
+		{ Foreground = { Color = "black" } },
+		{ Text = "" },
+	}
+end)
+
+return config
