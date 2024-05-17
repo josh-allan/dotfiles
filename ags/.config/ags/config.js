332003f325f1ec83875b420c49cded607ad700f1
Josh Allan <joshua.allan92@gmail.com>
2024-05-18 07:29:06 +1000

    fix broken references to hyprland env vars making the mouse cursor disappear

--- ags/.config/ags/config.js
+++ ags/.config/ags/config.js
@@ -1,10 +1,10 @@
 import { exec } from "resource:///com/github/Aylur/ags/utils.js";
 import Bar from "./modules/bar/index.js";
 import {
-  CornerTopleft,
-  CornerTopright,
-  CornerBottomright,
-  CornerBottomleft,
+    CornerTopleft,
+    CornerTopright,
+    CornerBottomright,
+    CornerBottomleft,
 } from "./modules/roundedCorner/index.js";
 import { IndicatorWidget } from "./modules/indicator/index.js";
 import Sidepanel from "./modules/sidepanel/index.js";
@@ -39,35 +39,36 @@ globalThis.SystemTray = SystemTray;
 exec(`sassc ${App.configDir}/scss/main.scss ${App.configDir}/style.css`);
 
 const applyScss = () => {
-  // Compile scss
-  exec(`sassc ${App.configDir}/scss/main.scss ${App.configDir}/style.css`);
-  console.log("Scss compiled");
+    // Compile scss
+    exec(`sassc ${App.configDir}/scss/main.scss ${App.configDir}/style.css`);
+    console.log("Scss compiled");
 
-  // Apply compiled css
-  App.resetCss();
-  App.applyCss(`${App.configDir}/style.css`);
-  console.log("Compiled css applied");
+    // Apply compiled css
+    App.resetCss();
+    App.applyCss(`${App.configDir}/style.css`);
+    console.log("Compiled css applied");
 };
 
 DirectoryMonitorService.connect("changed", () => applyScss());
 
 export default {
-  style: `${App.configDir}/style.css`,
-  closeWindowDelay: {
-    sideright: 350,
-    launcher: 350,
-    bar0: 350,
-  },
-  windows: [
-    Bar(0),
-    CornerTopleft(),
-    CornerTopright(),
-    CornerBottomleft(),
-    CornerBottomright(),
-    IndicatorWidget(),
-    Sidepanel(),
-    Launcher(),
-    PowerMenu(),
-    PopupNotifications(),
-  ],
+    style: `${App.configDir}/style.css`,
+    closeWindowDelay: {
+        sideright: 350,
+        launcher: 350,
+        bar0: 350,
+    },
+    windows: [
+        Bar(0),
+        Bar(1),
+        CornerTopleft(),
+        CornerTopright(),
+        CornerBottomleft(),
+        CornerBottomright(),
+        IndicatorWidget(),
+        Sidepanel(),
+        Launcher(),
+        PowerMenu(),
+        PopupNotifications(),
+    ],
 };
