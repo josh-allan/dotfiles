620658a477d0fde88f185976acf80dd9bee0fd82
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-08-25 08:39:08 +1000

    markdown -> markview

--- nvim/.config/nvim/lua/base/plugins.lua
+++ nvim/.config/nvim/lua/base/plugins.lua
@@ -566,13 +566,27 @@ return {
 			require("configs.harpoon")
 		end,
 	},
-	{
-		"MeanderingProgrammer/markdown.nvim",
-		main = "render-markdown",
-		opts = {},
-		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-	},
+	-- {
+	-- 	"MeanderingProgrammer/markdown.nvim",
+	-- 	main = "render-markdown",
+	-- 	opts = {},
+	-- 	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+	-- 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- },
+    {
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+        -- You will not need this if you installed the
+        -- parsers manually
+        -- Or if the parsers are in your $RUNTIMEPATH
+        "nvim-treesitter/nvim-treesitter",
+
+        "nvim-tree/nvim-web-devicons"
+    },
+    },
 	{
 		{
 			"folke/trouble.nvim",
