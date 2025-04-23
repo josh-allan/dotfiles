bd8a038864ae07bb4fa56740194679b3ac0a1b3f
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2025-04-24 07:39:46 +1000

    some slight changes

---
+++ fish/.config/fish/user/env/env.fish
@@ -0,0 +1,6 @@
+set -gx FISH_HOME $HOME/.config/fish
+set -gx BAT_THEME Dracula
+set -gx SUDO_EDITOR nvim
+set -gx GOPRIVATE github.com/10gen
+set -gx PYENV_ROOT $HOME/.pyenv
+set -gx PIPX_DEFAULT_PYTHON (pyenv prefix)/bin/python
