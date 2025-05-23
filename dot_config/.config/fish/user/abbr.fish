93643c0924c1f5f578bb40f5d8892e5fb45535e5
Josh Allan <joshua.allan92@gmail.com>
2025-05-24 09:16:34 +1000

    backup script and neovim monorepo

--- dot_config/.config/fish/user/abbr.fish
+++ dot_config/.config/fish/user/abbr.fish
@@ -1,4 +1,4 @@
-abbr vim nvim # change to use nvim 
+abbr vim "NVIM_APPNAME=nvim-monorepo/jvim nvim"
 abbr cat bat # a better cat
 abbr cls clear
 abbr find fd # a better find
@@ -11,5 +11,4 @@ abbr sdr 'sudo reboot now'
 abbr sdn 'shutdown now'
 abbr top btop
 abbr htop btop
-abbr jvim "NVIM_APPNAME=jvim nvim"
 abbr ash "jak atlas ssh"
