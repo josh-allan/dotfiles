52ad2804ae1cbfb2ba5fb419558a1dc1d8ba5ed9
Josh Allan <joshua.allan92@gmail.com>
2025-05-31 07:08:54 +1000

    abbr and docker

--- dot_config/.config/fish/user/abbr.fish
+++ dot_config/.config/fish/user/abbr.fish
@@ -1,14 +1,15 @@
-abbr vim "NVIM_APPNAME=nvim-monorepo/jvim nvim"
+abbr ash "jak atlas ssh"
 abbr cat bat # a better cat
 abbr cls clear
-abbr find fd # a better find
 abbr ff 'open .'
+abbr find fd # a better find
 abbr ga 'git add .'
 abbr gc 'git commit -m'
 abbr gcob 'git checkout -b'
-abbr mkdir 'mkdir -p'
-abbr sdr 'sudo reboot now'
-abbr sdn 'shutdown now'
-abbr top btop
 abbr htop btop
-abbr ash "jak atlas ssh"
+abbr k kubectl
+abbr mkdir 'mkdir -p'
+abbr sdn 'shutdown now'
+abbr sdr 'sudo reboot now'
+abbr top btop
+abbr vim nvim
