030220cc56129b53a3da94a3ced6225a0e58ea01
Josh Allan <joshua.allan92@gmail.com>
2025-06-03 21:56:41 +1000

    feat: docker function

--- dot_config/.config/fish/user/functions/docker.fish
+++ dot_config/.config/fish/user/functions/docker.fish
@@ -6,3 +6,7 @@ function dclean
     docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
     docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 end
+
+function dockerupdate
+    docker-compose pull $(docker images --filter "dangling=false" --format "{{.Repository}}:{{.Tag}}" 2>/dev/null)
+end
