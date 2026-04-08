323e62c64f4175ee9f48a852e94889dc6522fa6c
Josh Allan <joshua.allan92@gmail.com>
2026-04-08 20:52:11 +1000

    feat: update configuration files and key mappings

--- dot_config/.config/fish/config.fish
+++ dot_config/.config/fish/config.fish
@@ -9,3 +9,8 @@ end
 zoxide init fish --cmd cd | source
 direnv hook fish | source
 pyenv init - fish | source
+export PATH="$HOME/.local/bin:$PATH"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
