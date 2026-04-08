174bd8b0fc4722cb42790a4929e33a07f26733f5
Josh Allan <joshua.allan92@gmail.com>
2026-04-08 20:52:11 +1000

    feat: update configuration files and key mappings

--- dot_config/.config/fish/user/abbr.fish
+++ dot_config/.config/fish/user/abbr.fish
@@ -6,10 +6,9 @@ abbr find fd # a better find
 abbr ga 'git add .'
 abbr gc 'git commit -m'
 abbr gcob 'git checkout -b'
-abbr htop btop
 abbr k kubectl
 abbr mkdir 'mkdir -p'
 abbr sdn 'shutdown now'
 abbr sdr 'sudo reboot now'
-abbr top btop
+abbr top htop
 abbr vim nvim
